63dfeed1cecbf35a7c9bea7c1c5155f4dd626e9d
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-27 09:30:45 +1000

    link to home added

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar=()=>{
     return(
         
     <nav className="bg-white p-2 flex justify-between">
-    <Link href="" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
+    <Link href="/" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
     <div>
       <div className="flex">
         <button
