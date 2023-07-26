3a48398e926148a058995055dab5458a96c63577
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-27 09:24:26 +1000

    added search component from search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import NavBar from "../components/NavBar";
-import Header from "../components/Header";
+import Header from "./components/Header";
 
 const SearchPage=()=>{
     return(
         <main className="bg-gray-100 min-h-screen w-screen">
   <main className="max-w-screen-2xl m-auto bg-white">
     <NavBar/>
-
    <Header/>
     <div className="flex py-4 m-auto w-2/3 justify-between items-start">
       {/* SEARCH SIDE BAR */}
