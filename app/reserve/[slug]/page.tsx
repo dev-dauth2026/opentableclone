6daf7c10cc52263526a5a99100eb32300266f82f
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-26 12:41:36 +1000

    added link

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -6,7 +6,7 @@ const Reserve=()=>{
   <main className="max-w-screen-2xl m-auto bg-white">
     {/* NAVBAR */}
     <nav className="bg-white p-2 flex justify-between">
-      <Link href="" className="font-bold text-gray-700 text-2xl">
+      <Link href="/" className="font-bold text-gray-700 text-2xl">
         {" "} OpenTable{" "}
       </Link>
       <div>
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -35,8 +35,8 @@ const RestaurantMenuPage=()=>{
       <div className="bg-white w-[100%] rounded p-3 shadow">
         {/* RESAURANT NAVBAR */}
         <nav className="flex text-reg border-b pb-2">
-          <a href="" className="mr-7"> Overview </a>
-          <a href="" className="mr-7"> Menu </a>
+          <Link href="/restaurant/milestones-grill" className="mr-7"> Overview </Link>
+          <Link href="/restaurant/milestones-grill/menu"className="mr-7"> Menu </Link>
         </nav>
         {/* RESAURANT NAVBAR */} {/* MENU */}
         <main className="bg-white mt-5">
