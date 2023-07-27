2a46da74ef9e2ed97dc2f4572a7c7f0251bb22e0
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-27 10:34:19 +1000

    Form Component added

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/app/components/NavBar";
 import Link from "next/link";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 const Reserve=()=>{
     return(
@@ -11,48 +12,7 @@ const Reserve=()=>{
       <div className="py-9 w-3/5 m-auto">
         <Header/>
          {/* FORM */}
-        <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="First name"
-          />
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="Last name"
-          />
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="Phone number"
-          />
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="Email"
-          />
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="Occasion (optional)"
-          />
-          <input
-            type="text"
-            className="border rounded p-3 w-80 mb-4"
-            placeholder="Requests (optional)"
-          />
-          <button
-            className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
-          >
-            Complete reservation
-          </button>
-          <p className="mt-4 text-sm">
-            By clicking “Complete reservation” you agree to the OpenTable Terms
-            of Use and Privacy Policy. Standard text message rates may apply.
-            You may opt out of receiving text messages at any time.
-          </p>
-        </div>
+       <Form/>
       </div>
     </div>
   </main>
