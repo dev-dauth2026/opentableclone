66a600db04ddac7a7f795eda26e83e933fd8a2cc
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-27 10:00:24 +1000

    Title component created

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -2,6 +2,7 @@ import NavBar from '@/app/components/NavBar';
 import Link from 'next/link'
 import Header from './components/Header';
 import RestaurantNavBar from './components/RestaurantNavBar';
+import Title from './components/Title';
 const RestaurantDetails=()=>{
     return(
         <main className="bg-gray-100 min-h-screen w-screen">
@@ -12,13 +13,10 @@ const RestaurantDetails=()=>{
     {/* HEADER */} {/* DESCRIPTION PORTION */}
     <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        {/* RESAURANT NAVBAR */}
+   
        <RestaurantNavBar/>
-        {/* RESAURANT NAVBAR */} {/* TITLE */}
-        <div className="mt-4 border-b pb-6">
-          <h1 className="font-bold text-6xl">Milesstone Grill</h1>
-        </div>
-        {/* TITLE */} {/* RATING */}
+      <Title/>
+        {/* RATING */}
         <div className="flex items-end">
           <div className="ratings mt-2 flex items-center">
             <p>*****</p>
