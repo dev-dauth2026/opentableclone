c0ad3f7f2acb4bf0eca94f4b3039fc37dc4c6a76
Dauth Gurung <dauth22grg22@gmail.com>
2023-07-27 10:26:47 +1000

    Menu component added

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/app/components/NavBar";
 import Link from "next/link";
 import Header from "../components/Header";
 import RestaurantNavBar from "../components/RestaurantNavBar";
+import Menu from "../components/Menu";
 
 const RestaurantMenuPage=()=>{
     return(
@@ -14,24 +15,7 @@ const RestaurantMenuPage=()=>{
       <div className="bg-white w-[100%] rounded p-3 shadow">
       <RestaurantNavBar/>
       {/* MENU */}
-        <main className="bg-white mt-5">
-          <div>
-            <div className="mt-4 pb-1 mb-1">
-              <h1 className="font-bold text-4xl">Menu</h1>
-            </div>
-            <div className="flex flex-wrap justify-between">
-              {/* MENU CARD */}
-              <div className=" border rounded p-3 w-[49%] mb-3">
-                <h3 className="font-bold text-lg">Surf and Turf</h3>
-                <p className="font-light mt-1 text-sm">
-                  A well done steak with lobster and rice
-                </p>
-                <p className="mt-7">$80.00</p>
-              </div>
-              {/* MENU CARD */}
-            </div>
-          </div>
-        </main>
+      <Menu/>
         {/* MENU */}
       </div>
     </div>
