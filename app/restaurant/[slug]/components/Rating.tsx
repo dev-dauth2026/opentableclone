bdb7317069fe8612872d92031f2b1302eb5b9a02
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-04 08:11:19 +1000

    rating calculation added

--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -2,16 +2,20 @@ import Stars from "@/app/components/Stars";
 import { CalculateReviewsRatingAverage } from "@/app/utils/calculateRatingReviewsAverage";
 import { Review } from "@prisma/client";
 
-export default function Rating({reviews}:{reviews:Review[]}) {
+export default function Rating({ reviews }: { reviews: Review[] }) {
   return (
     <div className="flex items-end">
-          <div className="ratings mt-2 flex items-center">
-            <Stars reviews={reviews} />
-            <p className="text-reg ml-3">{CalculateReviewsRatingAverage(reviews).toFixed(1)}</p>
-          </div>
-          <div>
-            <p className="text-reg ml-4">{reviews.length} Review{reviews.length>1&&"s"}</p>
-          </div>
-        </div>
-  )
+      <div className="ratings mt-2 flex items-center">
+        <Stars reviews={reviews} />
+        <p className="text-reg ml-3">
+          {CalculateReviewsRatingAverage(reviews).toFixed(1)}
+        </p>
+      </div>
+      <div>
+        <p className="text-reg ml-4">
+          {reviews.length} Review{reviews.length > 1 && "s"}
+        </p>
+      </div>
+    </div>
+  );
 }
