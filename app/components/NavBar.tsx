f083d455fef550dec5f285973f0be4b56f7d4e6e
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-04 08:09:54 +1000

    login signup model button added

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import LoginModal from "./AuthModal";
+import AuthModal from "./AuthModal";
 
 const NavBar=()=>{
     return(
@@ -7,12 +9,11 @@ const NavBar=()=>{
     <Link href="/" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
     <div>
       <div className="flex">
-        <button
-          className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-        >
-          Sign in
-        </button>
-        <button className="border p-1 px-4 rounded">Sign up</button>
+       
+        <AuthModal isSignin={true}/>
+        <AuthModal isSignin={false}/>
+
+
       </div>
     </div>
   </nav>
