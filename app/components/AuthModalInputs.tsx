8075894d48b007ac2fd1368a9ed1994ef2f06ed0
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-04 08:13:04 +1000

    login signup input created

---
+++ app/components/AuthModalInputs.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+interface Props{
+  inputs:{
+    firstName:string,
+    lastName:string,
+    email:string,
+    phone:string,
+    city:string,
+    password:string,
+  };
+  handleChangeInput:(e:React.ChangeEvent<HTMLInputElement>)=>void;
+  isSignin:boolean
+}
+export default function AuthModalInputs({inputs,handleChangeInput,isSignin}:Props) {
+  return (
+    <div>
+      {isSignin? null:(<div className="my-3 flex justify-between text-sm">
+        <input type="text" className="border rounded p-2 py-3 w-[49%]" placeholder="First Name" value={inputs.firstName} name='firstName' onChange={handleChangeInput}/>
+        <input type="text" className="border rounded p-2 py-3 w-[49%]" placeholder="Last Name" value={inputs.lastName} name='lastName' onChange={handleChangeInput}/>
+      </div>)}
+      <div className="my-3 flex justify-between text-sm">
+        <input type="text" className="border rounded p-2 py-3 w-full" placeholder="Email" value={inputs.email} name='email' onChange={handleChangeInput}/>
+      </div>
+      {isSignin? null:(<div className="my-3 flex justify-between text-sm">
+        <input type="text" className="border rounded p-2 py-3 w-[49%]" placeholder="Phone" value={inputs.phone}  name='phone' onChange={handleChangeInput}/>
+        <input type="text" className="border rounded p-2 py-3 w-[49%]" placeholder="City" value={inputs.city} name='city' onChange={handleChangeInput}/>
+      </div>)}
+      <div className="my-3 flex justify-between text-sm">
+        <input type="password" className="border rounded p-2 py-3 w-full" placeholder="Password" value={inputs.password} name='password' onChange={handleChangeInput}/>
+      </div>
+
+    </div>
+  )
+}
