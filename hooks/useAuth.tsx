0d64e18a4ee9aafe3523bf614a597675230fbd16
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-08 10:31:53 +1000

    useAuth created and useContext and axios used

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,49 @@
+import { AuthenticationContext } from "@/app/context/AuthContext";
+import axios from "axios";
+import { useContext } from "react";
+const useAuth = () => {
+  const { data, error, loading, setAuthState } = useContext(
+    AuthenticationContext
+  );
+  const signin = async ({
+    email,
+    password,
+  }: {
+    email: String;
+    password: String;
+  }) => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response);
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      });
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
+  const signup = () => {};
+
+  return {
+    signin,
+    signup,
+  };
+};
+export default useAuth;
