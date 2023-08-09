e4534d5ded1946b164c7f410c39947357570d1bf
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-10 08:47:34 +1000

    Loading error fix

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { AuthenticationContext } from "../context/AuthContext";
 
 const NavBar = () => {
-  const { data } = useContext(AuthenticationContext);
+  const { data, loading } = useContext(AuthenticationContext);
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="/" className="font-bold text-gray-700 text-2xl">
@@ -14,18 +14,20 @@ const NavBar = () => {
         OpenTable{" "}
       </Link>
       <div>
-        <div className="flex">
-          {data ? (
-            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-              Logout
-            </button>
-          ) : (
-            <>
-              <AuthModal isSignin={true} />
-              <AuthModal isSignin={false} />
-            </>
-          )}
-        </div>
+        {loading ? null : (
+          <div className="flex">
+            {data ? (
+              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+                Logout
+              </button>
+            ) : (
+              <>
+                <AuthModal isSignin={true} />
+                <AuthModal isSignin={false} />
+              </>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   );
