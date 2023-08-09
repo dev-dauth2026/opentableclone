3f21a46e8c0eee85f63d8b497402f6b305896641
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-10 08:38:26 +1000

    rendering logout button conditionally after signin

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,23 +1,33 @@
+"use client";
 import Link from "next/link";
 import LoginModal from "./AuthModal";
 import AuthModal from "./AuthModal";
+import { useContext } from "react";
+import { AuthenticationContext } from "../context/AuthContext";
 
-const NavBar=()=>{
-    return(
-        
+const NavBar = () => {
+  const { data } = useContext(AuthenticationContext);
+  return (
     <nav className="bg-white p-2 flex justify-between">
-    <Link href="/" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
-    <div>
-      <div className="flex">
-       
-        <AuthModal isSignin={true}/>
-        <AuthModal isSignin={false}/>
-
-
+      <Link href="/" className="font-bold text-gray-700 text-2xl">
+        {" "}
+        OpenTable{" "}
+      </Link>
+      <div>
+        <div className="flex">
+          {data ? (
+            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+              Logout
+            </button>
+          ) : (
+            <>
+              <AuthModal isSignin={true} />
+              <AuthModal isSignin={false} />
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </nav>
- 
-    )
-}
+    </nav>
+  );
+};
 export default NavBar;
