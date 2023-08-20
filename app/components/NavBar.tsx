bc26748f67fe23565ca5b61f744cd6aaf9ac1a8c
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-20 13:43:26 +1000

    added signout functionality

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,9 +4,12 @@ import LoginModal from "./AuthModal";
 import AuthModal from "./AuthModal";
 import { useContext } from "react";
 import { AuthenticationContext } from "../context/AuthContext";
+import useAuth from "@/hooks/useAuth";
 
 const NavBar = () => {
   const { data, loading } = useContext(AuthenticationContext);
+  const { signout } = useAuth();
+
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="/" className="font-bold text-gray-700 text-2xl">
@@ -17,8 +20,11 @@ const NavBar = () => {
         {loading ? null : (
           <div className="flex">
             {data ? (
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Logout
+              <button
+                className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
+                onClick={signout}
+              >
+                Sign out
               </button>
             ) : (
               <>
