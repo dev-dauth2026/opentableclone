63d3619d925eabbdb035f6a36e6cd71b86ab87a5
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-20 15:27:31 +1000

    added open_time and close_time properties

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,67 +1,69 @@
-import Header from './components/Header';
-import RestaurantNavBar from './components/RestaurantNavBar';
-import Title from './components/Title';
-import Rating from './components/Rating';
-import Description from './components/Description';
-import Images from './components/Images';
-import Reviews from './components/Reviews';
-import ReservationCard from './components/ReservationCard';
-import { Cuisine, PrismaClient, Review } from '@prisma/client';
-import { notFound } from 'next/navigation';
+import Header from "./components/Header";
+import RestaurantNavBar from "./components/RestaurantNavBar";
+import Title from "./components/Title";
+import Rating from "./components/Rating";
+import Description from "./components/Description";
+import Images from "./components/Images";
+import Reviews from "./components/Reviews";
+import ReservationCard from "./components/ReservationCard";
+import { Cuisine, PrismaClient, Review } from "@prisma/client";
+import { notFound } from "next/navigation";
 
+const prisma = new PrismaClient();
 
-const prisma =new PrismaClient();
-
-interface Restaurant{
-      id:number,
-      name:string,
-      description:string,
-      images:string[],
-      slug:string,
-      reviews:Review[]
-
+interface Restaurant {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  open_time: string;
+  close_time: string;
+  slug: string;
+  reviews: Review[];
 }
 
-const fetchRestaurantBySlug=async(slug:string):Promise<Restaurant>=>{
-  const restaurant =await prisma.restaurant.findUnique({
-    where:{
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
       slug,
     },
-    select:{
-      id:true,
-      name:true,
-      description:true,
-      images:true,
-      slug:true,
-      reviews:true,
-    }
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      slug: true,
+      reviews: true,
+      open_time: true,
+      close_time: true,
+    },
   });
-  if(!restaurant){
-     notFound()
+  if (!restaurant) {
+    notFound();
   }
   return restaurant;
-}
+};
 
-const RestaurantDetails=async({params}:{params:{slug:string}})=>{
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
   const restaurant = await fetchRestaurantBySlug(params.slug);
-  
-    return(
-  <>
-  
+
+  return (
+    <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-       <RestaurantNavBar slug={restaurant.slug}/>
-      <Title name={restaurant.name}/>
-      <Rating reviews={restaurant.reviews}/>
-      <Description description={restaurant.description}/>
-      <Images images={restaurant.images}/>
-      <Reviews reviews={restaurant.reviews}/>
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
+        <Rating reviews={restaurant.reviews} />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
+        <Reviews reviews={restaurant.reviews} />
       </div>
       <div className="w-[27%] relative text-reg">
-        <ReservationCard/>
+        <ReservationCard
+          openTime={restaurant.open_time}
+          closeTime={restaurant.close_time}
+        />
       </div>
-  
-
     </>
-    )
-}
+  );
+};
 export default RestaurantDetails;
