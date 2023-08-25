933d2ceeaab5863d280737d0fc62df2671c02030
Dauth Gurung <dauth22grg22@gmail.com>
2023-08-25 10:05:06 +1000

    built useAvailabilities hook

---
+++ hooks/useAvailabilities.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function useAvalabilities() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    partySize,
+    day,
+    time,
+  }: {
+    slug: string;
+    partySize: string;
+    day: string;
+    time: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(`http://localhost:3000/api/restaurant`, {
+        params: {
+          day,
+          time,
+          partySize,
+        },
+      });
+      setLoading(false);
+      setData(response.data);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.data.response.errorMessage);
+    }
+  };
+  return { loading, data, error, fetchAvailabilities };
+}
