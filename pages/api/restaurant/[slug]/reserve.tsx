43d4ca85e4f63bed8cece73fa9cef8649f723a8e
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-01 15:38:05 +1000

    added POST requeset method

--- pages/api/restaurant/[slug]/reserve.tsx
+++ pages/api/restaurant/[slug]/reserve.tsx
@@ -8,101 +8,103 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string;
-    day: string;
-    time: string;
-    partySize: string;
-  };
+  if (req.method === "POST") {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
+    };
 
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
-  if (!restaurant) {
-    return res.status(400).json({
-      errorMessage: "Restaurant not found.",
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+      },
     });
-  }
-  if (
-    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
-    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
-  ) {
-    return res.status(400).json({
-      errorMessage: "Restaurant is not open at that time",
-    });
-  }
-
-  const searchTimesWithTables = await findAvailableTable({
-    day,
-    time,
-    res,
-    restaurant,
-  });
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({
-      errorMessage: "Invalid data provided",
-    });
-  }
-
-  const searchTimeWithTables = searchTimesWithTables.find((t) => {
-    return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
-  });
-
-  if (!searchTimeWithTables) {
-    return res.status(400).json({
-      errorMessage: "No availability, cannot book.",
-    });
-  }
-
-  const tablesCount: { 2: number[]; 4: number[] } = {
-    2: [],
-    4: [],
-  };
-
-  searchTimeWithTables.tables.forEach((table) => {
-    if (table.seats === 2) {
-      tablesCount[2].push(table.id);
-    } else {
-      tablesCount[4].push(table.id);
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage: "Restaurant not found.",
+      });
     }
-  });
+    if (
+      new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+      new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+    ) {
+      return res.status(400).json({
+        errorMessage: "Restaurant is not open at that time",
+      });
+    }
 
-  const tablesToBook: number[] = [];
-  let seatsRemaining = parseInt(partySize);
+    const searchTimesWithTables = await findAvailableTable({
+      day,
+      time,
+      res,
+      restaurant,
+    });
 
-  while (seatsRemaining > 0) {
-    if (seatsRemaining >= 3) {
-      if (tablesCount[4].length) {
-        tablesToBook.push(tablesCount[4][0]);
-        tablesCount[4].shift();
-        seatsRemaining = seatsRemaining - 4;
+    if (!searchTimesWithTables) {
+      return res.status(400).json({
+        errorMessage: "Invalid data provided",
+      });
+    }
+
+    const searchTimeWithTables = searchTimesWithTables.find((t) => {
+      return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+    });
+
+    if (!searchTimeWithTables) {
+      return res.status(400).json({
+        errorMessage: "No availability, cannot book.",
+      });
+    }
+
+    const tablesCount: { 2: number[]; 4: number[] } = {
+      2: [],
+      4: [],
+    };
+
+    searchTimeWithTables.tables.forEach((table) => {
+      if (table.seats === 2) {
+        tablesCount[2].push(table.id);
       } else {
-        tablesToBook.push(tablesCount[2][0]);
-        tablesCount[2].shift();
-        seatsRemaining = seatsRemaining - 2;
+        tablesCount[4].push(table.id);
       }
-    } else {
-      if (tablesCount[2].length) {
-        tablesToBook.push(tablesCount[2][0]);
-        tablesCount[2].shift();
-        seatsRemaining = seatsRemaining - 2;
+    });
+
+    const tablesToBook: number[] = [];
+    let seatsRemaining = parseInt(partySize);
+
+    while (seatsRemaining > 0) {
+      if (seatsRemaining >= 3) {
+        if (tablesCount[4].length) {
+          tablesToBook.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        } else {
+          tablesToBook.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        }
       } else {
-        tablesToBook.push(tablesCount[4][0]);
-        tablesCount[4].shift();
-        seatsRemaining = seatsRemaining - 4;
+        if (tablesCount[2].length) {
+          tablesToBook.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        } else {
+          tablesToBook.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        }
       }
     }
-  }
 
-  return res.json({
-    tablesToBook,
-  });
+    return res.json({
+      tablesToBook,
+    });
+  }
 }
