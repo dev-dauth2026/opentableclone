4943d4aa6868d71fc208a580d0db650abd944366
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-01 15:20:41 +1000

    counting the tables based on seats

--- pages/api/restaurant/[slug]/reserve.tsx
+++ pages/api/restaurant/[slug]/reserve.tsx
@@ -62,7 +62,20 @@ export default async function handler(
     });
   }
 
+  const tablesCount: { 2: number[]; 4: number[] } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
   return res.json({
-    searchTimesWithTables,
+    tablesCount,
   });
 }
