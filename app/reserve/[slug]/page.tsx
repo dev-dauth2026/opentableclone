02e3532cb4afec815c48f7600080482bcf78e2df
Dauth Gurung <dauth22grg22@gmail.com>
2023-09-04 07:29:52 +1000

    extracting the query params

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -24,12 +24,23 @@ const fetchRestaurantBySlug = async (slug: string) => {
   return restaurant;
 };
 
-const Reserve = async ({ params }: { params: { slug: string } }) => {
+const Reserve = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) => {
   const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header image={restaurant.main_image} name={restaurant.name} />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         {/* FORM */}
         <Form />
       </div>
